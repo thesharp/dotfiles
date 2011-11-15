de5be8bfa42ca86eeb1aa7eeb867e7f879bdfbb3
Ilya A. Otyutskiy <sharp@thesharp.ru>
2011-11-15 21:36:10 +0400

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function dotfile {
+	if [ -f $HOME/.$1 ] ; then
+		echo "File exists: $1"
+	else
+		ln -s $HOME/dotfiles/$1 $HOME/.$1
+	fi
+}
+
+dotfile vimrc
+dotfile gitconfig
+dotfile bashrc
