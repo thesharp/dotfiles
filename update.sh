516e4f6187db8c4276fcaaaff1bfde33307123e6
Ilya Otyutskiy <ilya.otyutskiy@icloud.com>
2016-03-21 19:58:42 +0300

    update.sh: Add LaunchAgents support

--- update.sh
+++ update.sh
@@ -15,6 +15,19 @@ function dotfile {
 	fi
 }
 
+function dotlaunchagent {
+	if [ -f $HOME/Library/LaunchAgents/$1 ] ; then
+		if [ -L $HOME/Library/LaunchAgents/$1 ] ; then
+			echo "LaunchAgent already linked: $1"
+		else
+			echo "LaunchAgent exists: $1. Probably you want to remove it and run update.sh again."
+		fi
+	else
+		ln -s $HOME/dotfiles/LaunchAgents/$1 $HOME/Library/LaunchAgents/$1
+		echo "Linking file $1"
+	fi
+}
+
 function dotdir {
 	if [ -d $HOME/.$1 ] ; then
 		if [ -L $HOME/.$1 ] ; then
@@ -39,8 +52,12 @@ dotdir mutt
 dotdir tmux
 
 # Darwin-specific
-if [ $OS == "Darwin" ] && [ ! -e $HOME/.bash_profile ] ; then
-	ln -s $HOME/.bashrc $HOME/.bash_profile
+if [ $OS == "Darwin" ] ; then
+	if [ ! -e $HOME/.bash_profile ] ; then
+		ln -s $HOME/.bashrc $HOME/.bash_profile
+	fi
+	dotlaunchagent org.thesharp.sup-index-backup.plist
+	launchctl load $HOME/Library/LaunchAgents/org.thesharp.sup-index-backup.plist
 fi
 
 # OpenBSD-specific
