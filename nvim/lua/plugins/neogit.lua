2d0699fcf1bc0468fb4401f34197aae645916857
Ilya Otyutskiy <sharp@thesharp.org>
2024-11-02 02:23:28 +0200

    feat(nvim): add commit editor mappings

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -6,9 +6,16 @@ return {
   },
   opts = {
     commit_editor = {
-      kind = "tab"
+      kind = "tab",
+    },
+    mappings = {
+      commit_editor = {
+        ["<c-p>"] = "PrevMessage",
+        ["<c-n>"] = "NextMessage"
+      }
     },
     use_per_project_settings = false,
     disable_insert_on_commit = true,
+    staged_diff_split_kind = "auto"
   }
 }
